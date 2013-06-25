f7c38bd3ca8f13b0cdd8f588a30cd45c10216b60
Pedro Begalli <pedrobegalli@ufmg.br>
2013-06-25 13:59:14 -0300

    dfgdsg

--- TesteGitHub/src/com/begalli/gitHub/Class2_1.java
+++ TesteGitHub/src/com/begalli/gitHub/Class2_1.java
@@ -4,5 +4,9 @@ public class Class2_1 {
 	public void teste(){
 		System.out.println("Class2_1 - Branch 1.1");
 		System.out.println("Class2_1 - Branch 1.1");
+		System.out.println("Class2_1 - Branch 1.1");
+		System.out.println("Class2_1 - Branch 1.1");
+		System.err.println("Class2_1 - Branch 1.1");
+		System.err.println("Class2_1 - Branch 1.1");
 	}
 }
