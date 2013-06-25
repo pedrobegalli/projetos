6ae7aa932421655491224bb3c64541faec502efc
Pedro Begalli <pedrobegalli@ufmg.br>
2013-06-25 14:01:23 -0300

    dsgdfg

--- TesteGitHub/src/com/begalli/gitHub/Class2_1.java
+++ TesteGitHub/src/com/begalli/gitHub/Class2_1.java
@@ -2,6 +2,7 @@ package com.begalli.gitHub;
 
 public class Class2_1 {
 	public void teste(){
+		System.err.println("Pedro BEgalli");
 		System.out.println("Class2_1 - Branch 1.1");
 		System.out.println("Class2_1 - Branch 1.1");
 	}
