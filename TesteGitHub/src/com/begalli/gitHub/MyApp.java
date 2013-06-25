4b5b9fb921106b71e2f13f8e396cd380b09e118a
Pedro Begalli <pedrobegalli@ufmg.br>
2013-06-25 13:39:58 -0300

    Initial

---
+++ TesteGitHub/src/com/begalli/gitHub/MyApp.java
@@ -0,0 +1,13 @@
+package com.begalli.gitHub;
+
+public class MyApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitHub is cool");
+	}
+
+}
