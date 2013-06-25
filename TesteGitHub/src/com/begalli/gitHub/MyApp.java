fe1f78cc3008e4fc0361dfbebb43d1fd76fd7f6e
Pedro Begalli <pedrobegalli@ufmg.br>
2013-06-25 13:42:19 -0300

    afadfsadfdsaf

--- TesteGitHub/src/com/begalli/gitHub/MyApp.java
+++ TesteGitHub/src/com/begalli/gitHub/MyApp.java
@@ -8,6 +8,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GitHub is cool");
+		System.out.println("GitHub is fun");
 	}
 
 }
